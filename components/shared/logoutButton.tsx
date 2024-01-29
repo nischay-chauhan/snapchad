f3b1157d61e49ee789deb221c6b65a0bd05dcd12
nischay-chauhan <masternischay@outlook.com>
2024-01-29 21:56:22 +0530

    logout

--- components/shared/logoutButton.tsx
+++ components/shared/logoutButton.tsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import { Button } from '../ui/button'
-import { LogOut } from 'lucide-react'
+import React from "react";
+import { Button } from "../ui/button";
+import { LogOut } from "lucide-react";
+import { signOut } from "@/auth";
 
 const LogoutButton = () => {
+  async function logoutAction(){
+    "use server"
+    await signOut()
+  }
   return (
-    <Button className='bg-black text-white rounded-full p-3 text-xs md:text-sm'>
-            <LogOut className='cursor-pointer' />
-          </Button>
-  )
-}
+    <form action={logoutAction}>
+      <Button className="bg-black text-white rounded-full p-3 text-xs md:text-sm">
+        <LogOut className="cursor-pointer" />
+      </Button>
+    </form>
+  );
+};
 
-export default LogoutButton
+export default LogoutButton;
